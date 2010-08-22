09062504e28e9b1355f10af39a081bcada409742
Tay Ray Chuan <rctay89@gmail.com>
2010-08-22 23:45:56 +0800

    js/Signals: don't pass event object to listeners
    It's an artifact of the underlying jquery implementation - just discard
    it, so that we don't affect the listener's expected arity.

--- js/Signals.js
+++ js/Signals.js
@@ -2,7 +2,10 @@ var NUSchedule = NUSchedule || {};
 NUSchedule.signals = NUSchedule.signals || {};
 
 NUSchedule.signals.register = function(signal, listener) {
-	$(document).bind(signal, listener);
+	$(document).bind(signal, function() {
+		// remove the first element, an event object
+		listener.apply(null, Array.prototype.slice.call(arguments, 1));
+	});
 };
 
 NUSchedule.signals.send = function(signal, args) {

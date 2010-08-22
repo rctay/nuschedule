82df297ed70316b0a008f1716187365ef443a51b
Tay Ray Chuan <rctay89@gmail.com>
2010-08-22 18:58:52 +0800

    add some test coverage for ripping

---
+++ test/ripping.js
@@ -0,0 +1,11 @@
+test("test ripping with CS1101S (Sem 1, 2010/2011) (cached)", function() {
+	// if the module was ripped correctly, then it will be appended and thus available at pos
+	var pos = tt.module.length;
+	ripper._send_request("data/2010-11-01-cs1101s.html");
+	stop(1000);
+	NUSchedule.signals.register("on_module_rip_success", function() {
+		start();
+		var module = tt.module[pos];
+		equals("CS1101S", module.code);
+	});
+});

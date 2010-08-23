aee7bdbd15e906951e3836e4bc323b0c83e3ccd9
Tay Ray Chuan <rctay89@gmail.com>
2010-08-23 23:01:30 +0800

    test/ripping: run tests on live scrape
    Bump up timeout too, since we're connecting to the outside world.

--- test/ripping.js
+++ test/ripping.js
@@ -1,7 +1,7 @@
 var prepare_test_cs1101s = function() {
 	// if the module was ripped correctly, then it will be appended and thus available at pos
 	var pos = tt.module.length;
-	QUnit.stop(1000);
+	QUnit.stop(10000);
 	NUSchedule.signals.register("on_module_rip_success", function() {
 		NUSchedule.signals.remove("on_module_rip_success", this);
 		QUnit.start();
@@ -28,4 +28,25 @@ var prepare_test_cs1101s = function() {
 QUnit.test("test ripping with CS1101S (Sem 1, 2010/2011) (cached)", function() {
 	prepare_test_cs1101s();
 	ripper._send_request("data/2010-11-01-cs1101s.html");
+});
+
+QUnit.test("test ripping with CS1101S (Sem 1, 2010/2011) (live)", function() {
+	prepare_test_cs1101s();
+
+	/*
+	 * create and populate input elements used by ripper.
+	 *
+	 * note that these are not intended to be exact replicas of the input
+	 * elements used in the main interface; we only need to have data
+	 * available.
+	 */
+	$("body")
+	.append($('<input type="text" id="code1">').val("cs1101s").hide())
+	.append($('<input type="text" id="ay">').val("2010/2011").hide())
+	.append($('<input type="text" id="semester">').val("1").hide());
+
+	equals($("#code1").val(), "cs1101s", "check input construction");
+
+	ripper.rip_index = 1;
+	ripper.rip();
 });

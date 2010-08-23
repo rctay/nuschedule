f252d188c5c10d8b46f2c51ab5a1757c60f44b43
Tay Ray Chuan <rctay89@gmail.com>
2010-08-23 22:27:43 +0800

    js/Signals: support removal of listeners

--- js/Signals.js
+++ js/Signals.js
@@ -2,10 +2,23 @@ var NUSchedule = NUSchedule || {};
 NUSchedule.signals = NUSchedule.signals || {};
 
 NUSchedule.signals.register = function(signal, listener) {
-	$(document).bind(signal, function() {
-		// remove the first element, an event object
-		listener.apply(null, Array.prototype.slice.call(arguments, 1));
-	});
+	var f = function() {
+		listener.apply(
+			// set the listener's context to be us, the wrapper
+			f,
+			// remove the first element, an event object
+			Array.prototype.slice.call(arguments, 1));
+	};
+	$(document).bind(signal, f);
+	return f;
+};
+
+NUSchedule.signals.remove = function(signal, wrapper) {
+	$(document).unbind(signal, wrapper);
+};
+
+NUSchedule.signals.remove_all = function(signal) {
+	$(document).unbind(signal);
 };
 
 NUSchedule.signals.send = function(signal, args) {

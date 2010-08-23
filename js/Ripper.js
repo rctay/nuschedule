6b02721692fbd6d6122b434858eba1a161e532f8
Tay Ray Chuan <rctay89@gmail.com>
2010-08-23 21:25:37 +0800

    js/Ripper.ripLecture, ripTutorial, getModule: localize variables

--- js/Ripper.js
+++ js/Ripper.js
@@ -122,13 +122,13 @@ Ripper.prototype.getModule = function () {
 	var arrLaboratory = new Array();
 	var arrTutLab = this.ripTutorial();
 
-	for (i=0;i<arrTutLab.length;i++) {
+	for (var i=0;i<arrTutLab.length;i++) {
 		if (arrTutLab[i].type == 'lab') arrLaboratory.push(arrTutLab[i]);
 		if (arrTutLab[i].type == 'tut') arrTutorial.push(arrTutLab[i]);
 	}
 
 	//generating new module object
-	oModule = new Module();
+	var oModule = new Module();
 	oModule.code = moduleCode;
 	oModule.link = url;
 	oModule.exam = examDate;
@@ -143,26 +143,31 @@ Ripper.prototype.ripLecture = function() {
 
 	var $lectureTable = $("table.tableframe:eq(0) ~ table:eq(0)", this.$page);
 
-	arrLecture = new Array();
+	var arrLecture = new Array();
 
 	// if (! /No Lecture Class/.test(this.sPage)) { //has lecture
 		//ripping all the lectures
 		$("table", $lectureTable).each(function() {
-			sBlock = $("td", this).html();
+			var sBlock = $("td", this).html();
 
 			//splitting the arrblock, to get separated piece of data
-			arrBlock = sBlock.split('<br>');
-			title = arrBlock[0].trim().substring(3);
+			var arrBlock = sBlock.split('<br>');
+			var title = arrBlock[0].trim().substring(3);
 
 			//session manipulation
-			nSession = Math.floor(arrBlock.length/2)-1;
-			arrSession = new Array();
-			for (i=0;i<nSession;i++) {
+			var nSession = Math.floor(arrBlock.length/2)-1;
+			var arrSession = new Array();
+
+			var phrase1, phrase2, arrCell;
+			var res, day, start, end, place;
+			var type, oSession;
+
+			for (var i=0;i<nSession;i++) {
 				phrase1 = arrBlock[i*2+1];
 				phrase2 = arrBlock[i*2+2];
 				arrCell = new Array();
 
-				var res = /(\w+)\s+From\s+(\d+)\s+hrs\s+to\s+(\d+)\s+hrs\s+in\s+(.+),/.exec(phrase1);
+				res = /(\w+)\s+From\s+(\d+)\s+hrs\s+to\s+(\d+)\s+hrs\s+in\s+(.+),/.exec(phrase1);
 				day = convertDay(res[1]);
 				start = parseInt(res[2]);
 				end = parseInt(res[3]);
@@ -180,7 +185,7 @@ Ripper.prototype.ripLecture = function() {
 					phrase2.indexOf("ODD") != -1 ? 1 : 0;
 
 				//pushing cells that this session will occupy
-				for (t=start;t<end;t+=100) { arrCell.push('w'+day+'t'+t); }
+				for (var t=start;t<end;t+=100) { arrCell.push('w'+day+'t'+t); }
 
 				//creating the particular session object, and push into the lecture.
 				oSession = new Session(day,start,end,type,place,arrCell);
@@ -200,29 +205,34 @@ Ripper.prototype.ripTutorial = function() {
 
 	var $tutorialTable = $("table.tableframe:eq(0) ~ table:eq(1)", this.$page);
 
-	arrTutorial = new Array();
+	var arrTutorial = new Array();
 
 	// if (! /No Tutorial Class/.test(this.sPage)) { //has tutorial
 		//ripping all the tutorials
 		$("table", $tutorialTable).each(function() {
-			sBlock = $("td", this).html();
+			var sBlock = $("td", this).html();
 
 			//splitting the arrblock, to get separated piece of data
-			arrBlock = sBlock.split('<br>');
-			title = arrBlock[0].trim().substring(3);
+			var arrBlock = sBlock.split('<br>');
+			var title = arrBlock[0].trim().substring(3);
 
 			//tutorial type
-			tutType = title.indexOf("LABORATORY") != -1 ? 'lab' : 'tut';
+			var tutType = title.indexOf("LABORATORY") != -1 ? 'lab' : 'tut';
 
 			//session manipulation
-			nSession = Math.floor(arrBlock.length/2)-1;
-			arrSession = new Array();
-			for (i=0;i<nSession;i++) {
+			var nSession = Math.floor(arrBlock.length/2)-1;
+			var arrSession = new Array();
+
+			var phrase1, phrase2, arrCell;
+			var res, day, start, end, place;
+			var type, oSession;
+
+			for (var i=0;i<nSession;i++) {
 				phrase1 = arrBlock[i*2+1];
 				phrase2 = arrBlock[i*2+2];
 				arrCell = new Array();
 
-				var res = /(\w+)\s+From\s+(\d+)\s+hrs\s+to\s+(\d+)\s+hrs\s+in\s+(.+),/.exec(phrase1);
+				res = /(\w+)\s+From\s+(\d+)\s+hrs\s+to\s+(\d+)\s+hrs\s+in\s+(.+),/.exec(phrase1);
 				day = convertDay(res[1]);
 				start = parseInt(res[2]);
 				end = parseInt(res[3]);
@@ -232,7 +242,7 @@ Ripper.prototype.ripTutorial = function() {
 					phrase2.indexOf("ODD") != -1 ? 1 : 0;
 
 				//pushing cells that this session will occupy
-				for (t=start;t<end;t+=100) { arrCell.push('w'+day+'t'+t); }
+				for (var t=start;t<end;t+=100) { arrCell.push('w'+day+'t'+t); }
 
 				//creating the particular session object, and push into the tutorial
 				oSession = new Session(day,start,end,type,place,arrCell);

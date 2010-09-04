e81465a7a2a1732623315030b47674da2b368bdd
Tay Ray Chuan <rctay89@gmail.com>
2010-09-05 01:31:17 +0800

    js/Dragger/showAvailableCell: change variable names, add module variable

--- js/Dragger.js
+++ js/Dragger.js
@@ -116,22 +116,23 @@ function showAvailableCell() {
 	var tid = dragElem.attr('id'); //like: n_0_tut_1
 	var arrtid = tid.split('_');
 	//rip
-	var md = parseInt(arrtid[1]); //module id
+	var module_id = parseInt(arrtid[1]);
+	var module = tt.module[module_id];
 
 	switch (arrtid[2]) {
 	case 'lec':
-		for (var p=0; p < tt.module[md].lecture.length; p++) {
-			tt.showNode(tt.module[md].code, tt.module[md].lecture[p], md, p);
+		for (var p=0; p < module.lecture.length; p++) {
+			tt.showNode(module, module.lecture[p], module_id, p);
 		}
 		break;
 	case 'tut':
-		for (var p=0; p < tt.module[md].tutorial.length; p++) {
-			tt.showNode(tt.module[md].code, tt.module[md].tutorial[p], md, p);
+		for (var p=0; p < module.tutorial.length; p++) {
+			tt.showNode(module.code, module.tutorial[p], module_id, p);
 		}
 		break;
 	case 'lab':
-		for (var p=0; p < tt.module[md].laboratory.length; p++) {
-			tt.showNode(tt.module[md].code, tt.module[md].laboratory[p], md, p);
+		for (var p=0; p < module.laboratory.length; p++) {
+			tt.showNode(module.code, module.laboratory[p], module_id, p);
 		}
 		break;
 	}
